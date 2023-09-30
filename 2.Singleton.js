44c175309069bc0d5425e7e2d04cb49bf6e055f6
Vignesh <85957001+vigneshactech2015@users.noreply.github.com>
2023-09-30 10:50:01 +0530

    Create 2.Singleton.js

---
+++ 2.Singleton.js
@@ -0,0 +1,37 @@
+/*
+  Singleton is a creational patterns
+  It is useful when we want to limit the number of instances created for a object to utmost one
+*/
+
+function Process(state){
+  this.state = state
+}
+
+//IIFE Function
+const Singleton = (function(){
+  function ProcessManager(){
+    this.numProcess = 0
+  }
+
+  let pManager
+
+  function createProcessManager(){
+    pManager = new ProcessManager()
+    return pManager
+  }
+
+  return {
+    getProcessManager : () => {
+      if(!pManager){
+        pManager = createProcessManager()
+      }else{
+        return pManager
+      }
+    }
+  }
+})()
+
+const processManager = Singleton.getProcessManager();
+const processManager2 = Singleton.getProcessManager();
+
+console.log(processManager === processManager2)
