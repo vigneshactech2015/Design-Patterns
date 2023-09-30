8042cce2399c24912ecb16e90c13b8d39f9d6829
Vignesh <85957001+vigneshactech2015@users.noreply.github.com>
2023-09-30 10:56:27 +0530

    Update 2.Singleton.js

--- 2.Singleton.js
+++ 2.Singleton.js
@@ -3,36 +3,29 @@
   It is useful when we want to limit the number of instances created for a object to utmost one
 */
 
-function Process(state){
-  this.state = state
-}
-
-//IIFE Function
+//It is also example of IIFE function
 const Singleton = (function(){
-  function ProcessManager(){
-    this.numProcess = 0
-  }
-
   let pManager
 
-  function createProcessManager(){
+  function ProcessManager() { /*...*/ }
+
+  function createProcessManager()
+  {
     pManager = new ProcessManager()
     return pManager
   }
 
   return {
-    getProcessManager : () => {
-      if(!pManager){
-        pManager = createProcessManager()
-      }else{
+      getProcessManager: () =>
+      {
+        if(!pManager)
+          pManager = createProcessManager()
         return pManager
       }
-    }
   }
 })()
 
-const processManager = Singleton.getProcessManager();
-const processManager2 = Singleton.getProcessManager();
+const singleton = Singleton.getProcessManager()
+const singleton2 = Singleton.getProcessManager()
 
-console.log(processManager === processManager2)
-//it returns true because it points to same instance and we are not creating two seperate instances
+console.log(singleton === singleton2) // true
